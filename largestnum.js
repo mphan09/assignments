015a39d34c4194c66210d5ee98300869724919ab
Melissa Phan <melissaphan09@yahoo.com>
2016-03-08 12:44:45 -0500

    Added files via upload

---
+++ largestnum.js
@@ -0,0 +1,10 @@
+var array = [40, 21, 100, 8, 43, 8];
+var largest= 0;
+
+for (i=0; i <= largest; i++){
+    if (array[i] > largest) {
+        var largest = array[i];
+    }
+}
+
+console.log(largest);
